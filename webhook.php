171e1cc4f7bb68320517743ba01ca6326198e17d
win12 <win@qq.com>
2020-07-08 02:20:35 +0800

    win12修改了webhoonk.php

--- webhook.php
+++ webhook.php
@@ -1,3 +1,17 @@
 <?php
-file_put_contents('hook.txt', $GLOBALS['HTTP_RAW_POST_DATA']);
+// GitHub Webhook Secret.
+// Keep it the same with the 'Secret' field on your Webhooks / Manage webhook page of your respostory.
+$secret = "webhook";
+// 项目根目录, 如: "/var/www/fizzday"
+$path = "C:\phpStudy\PHPTutorial\WWW\weworkapi_php-master";
+// Headers deliveried from GitHub
+$signature = $_SERVER['HTTP_X_HUB_SIGNATURE'];
+if ($signature) {
+    $hash = "sha1=" . hash_hmac('sha1', $HTTP_RAW_POST_DATA, $secret);
+    if (strcmp($signature, $hash) == 0) {
+        echo shell_exec("cd {$path} && /usr/bin/git reset --hard origin/master && /usr/bin/git clean -f && /usr/bin/git pull 2>&1");
+        exit();
+    }
+}
+http_response_code(404);
 ?>
