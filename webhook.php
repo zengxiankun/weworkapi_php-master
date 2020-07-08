4264154dcc67265649ee04cdb114a08c5860f4d5
win12 <win@qq.com>
2020-07-08 11:36:02 +0800

    aff12

--- webhook.php
+++ webhook.php
@@ -1,20 +1,51 @@
 <?php
-// GitHub Webhook Secret.
-// Keep it the same with the 'Secret' field on your Webhooks / Manage webhook page of your respostory.
-$secret = "webhook";
-// 项目根目录, 如: "/var/www/fizzday"
-$path = "C:\phpStudy\PHPTutorial\WWW\weworkapi_php-master";
-// Headers deliveried from GitHub
-//$signature = $_SERVER['HTTP_X_HUB_SIGNATURE'];
-//file_put_contents('hook.txt', $signature);
-//if ($signature) {
-//$HTTP_RAW_POST_DATA=$GLOBALS['HTTP_RAW_POST_DATA'];
-   // $hash = "sha1=" . hash_hmac('sha1', $HTTP_RAW_POST_DATA, $secret);
-    //if (strcmp($signature, $hash) == 0) {
-        //echo shell_exec("cd {$path} && git reset --hard origin/master && git clean -f && git pull 2>&1");
-       echo shell_exec("git pull");
-        exit();
-   // }
-//}
-//http_response_code(404);
-?>
+
+// 自行创建一个验证密码
+$keySecret = 'webhook';
+
+// 修改为你自己的仓库绝对路径
+$wwwRoot = [
+    'C:\phpStudy\PHPTutorial\WWW\weworkapi_php-master',
+];
+
+// 保存运行脚本的日志
+$logFile = 'webhook.log';
+
+// 执行git命令
+$gitCommand = 'git pull origin master';
+
+// 判断是否开启秘钥认证(已实现gitee和github)
+// if (isset($keySecret) && !empty($keySecret)) {
+//     list($headers, $gitType) = [[], null];
+//     foreach ($_SERVER as $key => $value) {
+//         'HTTP_' == substr($key, 0, 5) && $headers[str_replace('_', '-', substr($key, 5))] = $value;
+//         if (empty($gitType) && strpos($key, 'GITEE') !== false) {
+//             $gitType = 'GITEE';
+//         }
+//         if (empty($gitType) && strpos($key, 'GITHUB') !== false) {
+//             $gitType = 'GITHUB';
+//         }
+//     }
+//     if ($gitType == 'GITEE') {
+//         if (!isset($headers['X-GITEE-TOKEN']) || $headers['X-GITEE-TOKEN'] != $keySecret) {
+//             die('GITEE - 请求失败，秘钥有误');
+//         }
+//     } elseif ($gitType == 'GITHUB') {
+//         $json_content = file_get_contents('php://input');
+//         $signature = "sha1=" . hash_hmac('sha1', $json_content, $keySecret);
+//         if ($signature != $headers['X-HUB-SIGNATURE']) {
+//             die('GITHUB - 请求失败，秘钥有误');
+//         }
+//     } else {
+//         die('请求错误，未知git类型');
+//     }
+// }
+
+!is_array($wwwRoot) && $wwwRoot = [$wwwRoot];
+foreach ($wwwRoot as $vo) {
+    $shell = sprintf("cd %s && git pull 2>&1", $vo);
+    $output = shell_exec($shell);
+    $log = sprintf("[%s] %s \n", date('Y-m-d H:i:s', time()) . ' - ' . $vo, $output);
+    echo $log;
+    file_put_contents($logFile, $log, FILE_APPEND);
+}
